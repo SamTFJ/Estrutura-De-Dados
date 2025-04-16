81dad7dd29443305dccf7e3106a276741b269ef3
Samuel Jales <samueljjales@gmail.com>
2025-04-16 15:32:12 -0300

    Tabela finalizada com arquivo makefile e readme

---
+++ Tabela-Hash/README.md
@@ -0,0 +1,14 @@
+## Implementação da Tabela Hash em C++
+## Compilação
+### Para compilar o programa, use:
+```
+    make
+```
+### Para executar, use:
+```
+    make run
+```
+### Para deletar e limpar os arquivos criados, use:
+```
+    make clean
+```
--- Tabela-Hash/tabelahash.cpp
+++ Tabela-Hash/tabelahash.cpp
@@ -11,15 +11,24 @@ class TabelaHash {
     public:
 
         void inserir (int index, const vector<string>& valor) {
+            for (auto& elemento : tabelahash) {
+                if (elemento.first == index) {
+                    // Adiciona todos os nomes no vetor existente
+                    for (const auto& nome : valor) {
+                        elemento.second.push_back(nome);
+                    }
+                    return;
+                }
+            }
+            // Se não encontrou o índice, adiciona novo par
             tabelahash.push_back({index, valor});
         }
 
-        int funcaohash (int chave, string nome){
+        void funcaohash (int chave, string nome){
             int indice = chave % 10;
 
             inserir(indice, {nome});
         };
-        
 
         void printtable () {
             // imprime cada par, o compilador deduz cada elemento da table
@@ -31,19 +40,42 @@ class TabelaHash {
                 cout << endl;
             }
         }
+        
+        void encontrar (int chave, string nome1){
+            int indice = chave % 10;
 
+            for (const auto& elemento : tabelahash) {
+                if (elemento.first == indice){
+                    for(int i = 0; i < elemento.second.size(); ++i){
+                        if (elemento.second[i] == nome1){
+                            cout << "O elemento " << nome1 << " está no índice: " << elemento.first << " " << "e na posição: " << i << endl;   
+                        }
+                    }
+                }
+            }
+        }
 };
 
 int main (){
-    TabelaHash tbh;
+    TabelaHash tbh;    
 
     // Inserindo dados na tabela
-    tbh.inserir(0, {"João", "Pedro"});
-    tbh.inserir(1, {"Marco", "Matheus"});
-    tbh.inserir(2, {"Marina", "Juliana"});
+    tbh.funcaohash(5000, "gabriel");
+    tbh.funcaohash(6000, "Ronaldo");
+    tbh.funcaohash(7000, "Marcos");
+    tbh.funcaohash(5001, "Julia");
+    tbh.funcaohash(6001, "João");
+    tbh.funcaohash(7001, "Carlos");
+    tbh.funcaohash(5002, "Ronaebson");
+    tbh.funcaohash(5003, "Taís");
+    tbh.funcaohash(5004, "Samuel");
 
     // Impressão
     tbh.printtable();
+
+    // Procurando elementos
+    tbh.encontrar(7000, "Marcos");
+    tbh.encontrar(7001, "Carlos");
 
     return 0;
 }
